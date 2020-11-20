a2d2477c6963ba024655f294d3d65b4612b650ce
wzshiming <wzshiming@foxmail.com>
2020-11-20 17:48:21 +0800

    First commit

---
+++ go.mod
@@ -0,0 +1,9 @@
+module github.com/wzshiming/logger
+
+go 1.15
+
+require (
+	github.com/go-logr/logr v0.3.0
+	github.com/go-logr/zapr v0.3.0
+	go.uber.org/zap v1.16.0
+)
---
+++ log.go
@@ -0,0 +1,38 @@
+package logger
+
+import (
+	"context"
+
+	"github.com/go-logr/logr"
+)
+
+// DiscardLogger is a logr.Logger that does nothing.
+var DiscardLogger = logr.Discard()
+
+// Log is the base logger used by kubebuilder.  It delegates
+// to another logr.Logger.  You *must* call SetLogger to
+// get any actual logging.
+var Log = NewDelegatingLogger(DiscardLogger)
+
+// SetLogger sets a concrete logging implementation for all deferred Loggers.
+func SetLogger(l logr.Logger) {
+	Log.Fulfill(l)
+}
+
+// FromContext returns a logger with predefined values from a context.Context.
+func FromContext(ctx context.Context) logr.Logger {
+	if ctx == nil {
+		return Log
+	}
+	log := logr.FromContext(ctx)
+	if log == nil {
+		return Log
+	}
+	return log
+}
+
+// WithContext takes a context and sets the logger as one of its keys.
+// Use FromContext function to retrieve the logger.
+func WithContext(ctx context.Context, log logr.Logger) context.Context {
+	return logr.NewContext(ctx, log)
+}
